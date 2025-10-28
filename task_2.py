d81b4712dc9347cd7fa587276439bf0449ee19db
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2025-10-28 23:04:40 +0200

    feat: added solution files

---
+++ task_2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Завдання 2: Генератор дійсних чисел та обчислення прибутку
+
+Реалізація функції generator_numbers для ідентифікації дійсних чисел у тексті
+та функції sum_profit для обчислення загальної суми.
+"""
+
+import re
+from typing import Callable, Generator
+
+
+def generator_numbers(text: str) -> Generator[float, None, None]:
+
+    pattern = r'\b\d+\.\d+\b|\b\d+\b'
+
+    matches = re.finditer(pattern, text)
+
+    for match in matches:
+        yield float(match.group())
+
+
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+   
+    return sum(func(text))
+
