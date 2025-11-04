295de2164d18902ce40bfaa7e70442f4652031f4
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2025-11-04 23:24:33 +0200

    fix: fixed task2

--- task_2.py
+++ task_2.py
@@ -11,16 +11,14 @@ from typing import Callable, Generator
 
 
 def generator_numbers(text: str) -> Generator[float, None, None]:
-
-    pattern = r'\b\d+\.\d+\b|\b\d+\b'
-
+    pattern = r'(?<= )\d+\.\d+(?= )|(?<= )\d+(?= )'
+    
     matches = re.finditer(pattern, text)
-
+    
     for match in matches:
         yield float(match.group())
 
 
 def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
-   
     return sum(func(text))
 
